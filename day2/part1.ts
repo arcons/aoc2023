6dc82225dcb7aefe81bd9e3d5d6b3131d0ca4eab
Alex Consunji <arcon1993@gmail.com>
2024-01-07 22:14:35 -0500

    Dont commit dis

--- day2/part1.ts
+++ day2/part1.ts
@@ -6,6 +6,11 @@ let contents = fs.readFileSync('./day2/input.txt', 'utf8');
 //grab each line
 const input = contents.split(/\r?\n/);
 
+const colorMapMaxes = {
+    'red': 12,
+    'green' : 13,
+    'blue' : 14
+}
 
 // goal is to find which games are possible
 // each game is split by ;
@@ -13,36 +18,28 @@ const input = contents.split(/\r?\n/);
 // check to see if any of the values after splitting contain a value bigger than whats above
 // sum the ids of the games
 
-const gamePower = (gameSets: string) => {
+let sumOfIllegal = 0
+const illegalGameCheck = (gameSets: string) => {
     // match digits whitespace and words
     const findMatch = gameSets.match(/(\d+)\s+(\w+)/g)
     // this will contain the digits and their colors
-
-    const colorMapMaxes = {
-        'red': 0,
-        'green' : 0,
-        'blue' : 0
-    }
-
     for (let marbles of findMatch) {
         const [count ,color]= marbles.split(' ')
-        colorMapMaxes[color] = Math.max(colorMapMaxes[color], parseInt(count))
+        if(colorMapMaxes[color] < parseInt(count))
+        return false
     }
-    let power = 1
-    Object.values(colorMapMaxes).forEach(
-        val => {
-            power = power * val
-        }
-    )
-    return power;
+    return true
 }
 
-let sumOfPower = 0 
 input.forEach(game => {
     const currentGame =  game.split(':')
     const gameId = parseInt(currentGame[0].split(' ')[1])
-      sumOfPower += gamePower(currentGame[1])
+    if(illegalGameCheck(currentGame[1])) {
+      sumOfIllegal += gameId
+      console.log("illegal id ", gameId)
     }
-)
+    // quick checkerFunction
+    // 2751 too high
+})
 
-console.log(sumOfPower)
+console.log(sumOfIllegal)
